041944a8cf446e6c1b1cd52deacc8b7f0ed1be1b
monish koppa <mkoppa@mkoppa-a01.vmware.com>
2018-05-01 22:32:31 +0530

    adding comments to the class and methods

--- media.py
+++ media.py
@@ -4,13 +4,33 @@ import webbrowser
 
 
 class Movie:
-    # constructor for Movie object
+    """ Template for creating movie object
+
+    methods:
+    show_trailer()
+    """
     def __init__(self, title, story, img_url, trailer_url):
+    	""" Movie constructor
+        This is called when the Movie instance is created
+
+        Args:
+            title (str): Movie title
+            storyline (str): Movie Storyline
+            poster_image_url (str): url of image/poster
+            trailer_youtube_url (str): link to youtube
+        """
         self.title = title
         self.storyline = story
         self.poster_image_url = img_url
         self.trailer_youtube_url = trailer_url
 
-    # method to show the trailer
     def show_trailer(self):
+    	""" function show trailer
+
+        Args:
+            None
+        Returns:
+            None
+        """
         webbrowser.open(self.trailer)
+
